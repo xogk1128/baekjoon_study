9bd0d63628b2dc1cee3f69a5df5d1b2b8ce5aa7d
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2023-02-24 15:11:57 +0900

    [Bronze II] Title: 커트라인, Time: 296 ms, Memory: 20920 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void sort(int[] list, int size) {
+		int key;
+		int j;
+		for(int i=1; i<size; i++) {
+			key = list[i];
+			for(j=i-1; j>=0 && list[j]>key; j--)
+				list[j+1] = list[j];
+			list[j+1] = key;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int k = sc.nextInt();
+		int[] list = new int[N];
+		for(int i=0; i<N; i++)
+			list[i] = sc.nextInt();
+		sort(list, N);
+		System.out.println(list[N-k]);
+	}
+
+}
