fe48a1e0557772615bc8a7c17f490aec4148e2fe
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2023-03-03 16:20:35 +0900

    [Bronze V] Title: 팩토리얼, Time: 124 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int recursion(int num){
+        if(num > 1)
+            return num * recursion(num-1);
+        else return 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(recursion(N));
+    }
+}
