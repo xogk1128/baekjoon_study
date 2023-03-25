f36d1ba47acfd46ce1fcb910dd9f9d99bfbb752d
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2023-03-25 19:40:13 +0900

    [Silver V] Title: 소수 찾기, Time: 128 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        int cnt = 0;
+        for(int i=0; i<N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            for(int j=2; j<=num/2; j++){
+                if(num%j == 0){
+                    cnt--;
+                    break;
+                }
+
+            }
+            if(num != 1)
+                cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
